16bf5311c0439a4120524cc936ecbc5dc18bd5b0
“Carlos <ing.josecarlos@gmail.com>
2022-10-28 14:37:47 -0600

    78-80-CardWidget-ArgumentosWidgets

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -11,8 +11,43 @@ class CardScreen extends StatelessWidget {
         title: const Text('Card Widget'),
       ),
       body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         children: const [
           CustomCardType1(),
+          SizedBox(
+            height: 20,
+          ),
+          CustomCardType2(
+              imageUrl:
+                  'https://rare-gallery.com/mocahbig/1357322-MountainArtistic-Landscape-4k-Ultra-HD-Wallpaper.jpg',
+              name: "1"),
+          SizedBox(
+            height: 20,
+          ),
+          CustomCardType2(
+              imageUrl:
+                  'https://cdn3.dpmag.com/2021/07/Landscape-Tips-Mike-Mezeul-II.jpg'),
+          SizedBox(
+            height: 20,
+          ),
+          CustomCardType2(
+              imageUrl:
+                  'https://cdn3.dpmag.com/2021/07/Landscape-Tips-1-Mike-Mezeul-II.jpg'),
+          SizedBox(
+            height: 20,
+          ),
+          CustomCardType2(
+              imageUrl:
+                  'https://cdn3.dpmag.com/2021/07/Landscape-Tips-2-Mike-Mezeul-II.jpg'),
+          SizedBox(
+            height: 20,
+          ),
+          CustomCardType2(
+              imageUrl:
+                  'https://cdn3.dpmag.com/2021/07/Landscape-Tips-3-Mike-Mezeul-II.jpg'),
+          SizedBox(
+            height: 10,
+          ),
         ],
       ),
     );
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,1 +1,2 @@
 export 'package:fl_components/widgets/Custom_Card_Type_1.dart';
+export 'package:fl_components/widgets/Custom_Card_Type_2.dart';
